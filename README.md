c864107117feba946d268c15a6cc8cbde68916a2
Dima Gerasimov <karlicoss@gmail.com>
2017-11-04 15:23:44 +0000

    KLM support

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ import CSV on google maps (4sq)
 
 # Requirements
 ```pip3 install foursquare```
+
+# Adding maps
+* https://www.google.com/maps/d/u/0/home?hl=en
+
+# TODO
+* ugh apparently there is no way to add places via google maps api :( https://issuetracker.google.com/issues/35820262
--- fsq2gmaps/main.py
+++ fsq2gmaps/main.py
@@ -19,23 +19,31 @@ print("LISTS:")
 for k in lmap:
     print(k)
 
+
+class Place(NamedTuple):
+    lst: str
+    name: str
+    address: str
+    lat: float
+    lon: float
+
 # TODO use layers?
-def gen_places(lst) -> Iterable[Dict[str, str]]:
-    pprint(lst)
+def gen_places(lst) -> Iterable[Place]:
+    print(f"Scanning list: {lst['name']}")
     ldet = api.lists(lst['id'])
-    pprint(ldet)
     # TODO get all of them, might hit the limit
     items = ldet['list']['listItems']['items']
-    pprint(len(items))
+    print(f"{len(items)} items")
 
     for item in items:
         venue = item['venue']
-        yield {
-            "Name": venue['name'],
-            "Address": venue['location']['address'],
-            "Latitude": venue['location']['lat'],
-            "Longitude": venue['location']['lng'],
-        }
+        yield Place(
+            lst=lst['name'],
+            name=venue['name'],
+            address=venue['location']['address'],
+            lat=venue['location']['lat'],
+            lon=venue['location']['lng'],
+        )
 
 
 def gen_all_places():
@@ -43,12 +51,73 @@ def gen_all_places():
         for p in gen_places(lmap[lname]):
             yield p
 
+INTERESTING = ('london-todo', 'london-food', 'london',)
+
+def get_color(s: str):
+    hash(s)
+
+# pip install fastkml, shapely
+import fastkml # type: ignore
+K = fastkml
+from shapely.geometry import Point
+
+# TODO generate each color
+# TODO nicer, declarative DSL for building that crap
+def build_kml():
+    # Create the root KML object
+    kml = K.KML()
+    ns = '{http://www.opengis.net/kml/2.2}'
+    
+    # Create a KML Document and add it to the KML root object
+    doc = K.Document(ns, 'docid', 'doc name', 'doc description')
+
+    style = K.Style(ns=ns, id="red-normal")
+    # TODO ugh, it's aabbggrr
+    red = K.styles.IconStyle(ns=ns, color="ff000000")
+    red.icon_href = "http://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png"
+    # TODO ugh, do I really ned url here?
+    style.append_style(red)
+    doc.append_style(style)
+
+    style_map = K.StyleMap(
+        ns=ns,
+        id="style-red",
+    )
+    style_map.normal = K.StyleUrl(ns=ns, url="#red-normal")
+    style_map.highlight = K.StyleUrl(ns=ns, url="#red-normal")
+    doc.append_style(style_map)
+
+
+    for lname in INTERESTING:
+        # Create a KML Folder and add it to the Document
+        folder = K.Folder(ns=ns, id='fid', name=lname)
+        for p in gen_places(lmap[lname]):
+            pm = K.Placemark(ns=ns, id='id', name=p.name, description=p.address)
+            pm.geometry = Point(p.lon, p.lat)
+            pm.styleUrl = f"#style-red"
+            folder.append(pm)
+        doc.append(folder)
+
+    kml.append(doc)
+    return kml
+
+
+kml = build_kml()
+
+
+with open("res.kml", 'w') as fo:
+    import sys
+    sys.stdout.write(kml.to_string(prettyprint=True))
+    fo.write(kml.to_string(prettyprint=True))
+
+raise RuntimeError
 with open("res.csv", 'w') as fo:
     writer = csv.DictWriter(fo, fieldnames=[
         "Name",
         "Address",
         "Latitude",
-        "Longitude"
+        "Longitude",
+        "Color",
     ])
     writer.writeheader()
     for p in gen_all_places():
