89bc1dee331adf572182ca0ba408187b16b57410
Dima Gerasimov <karlicoss@gmail.com>
2019-06-30 10:45:58 +0100

    Render Folium map

--- fsq2gmaps/__main__.py
+++ fsq2gmaps/__main__.py
@@ -2,14 +2,23 @@
 import argparse
 import functools
 import sys
+import logging
 from datetime import datetime
 from pathlib import Path
 from pprint import pprint
-from typing import Dict, Iterable, List, NamedTuple, Optional
+from tempfile import TemporaryDirectory
+from typing import Dict, Iterable, List, NamedTuple, Optional, Any
+
+from kython.klogging import LazyLogger, setup_logzero
 
 import fastkml as K
 import webcolors
 from shapely.geometry import Point
+
+
+logger = LazyLogger('fsq2gmaps')
+
+LONDON = [51.538, -0.14]
 
 
 @functools.lru_cache(1)
@@ -20,7 +29,7 @@ def get_4sq_api():
     return api
 
 
-def get_4sq_data(): # TODO eh, should get local?
+def get_4sq_lists(): # TODO eh, should get local?
     api = get_4sq_api()
     lists = api.users.lists() # pylint: disable=no-member
 
@@ -37,41 +46,67 @@ def get_4sq_data(): # TODO eh, should get local?
     return lmap
 
 
-class Place(NamedTuple):
-    lst: str
-    name: str
-    address: str
-    lat: float
-    lon: float
-
-# TODO use layers?
-def gen_places(lst) -> Iterable[Place]:
-    api = get_4sq_api()
-    print(f"Scanning list: {lst['name']}")
-    ldet = api.lists(lst['id']) # pylint: disable=no-member
-    # TODO get all of them, might hit the limit
-    items = ldet['list']['listItems']['items']
-    print(f"{len(items)} items")
-
-    for item in items:
-        venue = item['venue']
-        yield Place(
-            lst=lst['name'],
-            name=venue['name'],
-            address=venue['location'].get('address', 'NO ADDRESS!'),
-            lat=venue['location']['lat'],
-            lon=venue['location']['lng'],
-        )
-
-
 # TODO move to config?
 INTERESTING = {
     'my saved places': 'red',
     'my liked places': 'pink',
-    # 'london-food': None,
+    'london-food': None,
     'london': 'blue',
     'london-todo': 'red',
 }
+
+
+class Place(NamedTuple):
+    lst: str
+    jvenue: Any
+    color: Optional[str]=None
+
+    @property
+    def name(self) -> str:
+        return self.jvenue['name']
+
+    @property
+    def address(self) -> str:
+        return ' '.join(self.jvenue['location'].get('formattedAddress', []))
+
+    @property
+    def lat(self):
+        return self.jvenue['location']['lat']
+
+    @property
+    def lng(self):
+        return self.jvenue['location']['lng']
+
+    @property
+    def descripton(self) -> str:
+        return f"{self.name}\n{self.address}\nList: {self.lst}"
+
+
+# TODO use layers?
+def gen_places() -> Iterable[Place]:
+    api = get_4sq_api()
+    lists = get_4sq_lists()
+
+    for lname, lst in lists.items():
+        if lname not in INTERESTING:
+            logger.info('skipping list %s', lname)
+            continue
+        # TODO warn about unused??
+        color = INTERESTING[lname]
+
+        print(f"Scanning list: {lst['name']}")
+        ldet = api.lists(lst['id']) # pylint: disable=no-member
+        # TODO get all of them, might hit the limit
+        items = ldet['list']['listItems']['items']
+        print(f"{len(items)} items")
+
+        for item in items:
+            venue = item['venue']
+            yield Place(
+                lst=lst['name'],
+                jvenue=venue,
+                color=color,
+            )
 
 
 # TODO er... whre is it coming from?
@@ -160,19 +195,21 @@ class KmlMaker:
 # TODO generate each color
 # TODO nicer, declarative DSL for building that crap
 def build_kml() -> KmlMaker:
-    lmap = get_4sq_data()
     kml = KmlMaker()
-    for lname, color in INTERESTING.items():
+    from kython import group_by_key
+    items = gen_places()
+    for lname, places in group_by_key(items, key=lambda p: p.lst).items():
+        color = places[0].color
         style_url = None if color is None else kml.make_icon_style(color=color)
         marks = []
-        for p in gen_places(lmap[lname]):
+        for p in places:
             pm = K.Placemark(
                 id=p.name,
                 name=p.name,
-                description=f"List: {lname}\n{p.address}",
+                description=p.description,
                 styleUrl=style_url,
             )
-            pm.geometry = Point(p.lon, p.lat)
+            pm.geometry = Point(p.lng, p.lat)
             marks.append(pm)
         kml.add_folder(
             name=lname,
@@ -182,13 +219,107 @@ def build_kml() -> KmlMaker:
     return kml
 
 
+def get_test_places():
+    return [
+        Place(
+            lst='whatever',
+            jvenue={
+                'location': {
+                    'formattedAddress': ['London', 'UK', 'Shoreditch'],
+                    'lat': 11.123,
+                    'lng': 0.4,
+                },
+                'name': 'Some place',
+            },
+        )
+    ]
+
+
+
 def get_kml() -> str:
     kml = build_kml()
     ss = kml.to_string(prettyprint=True)
     return '<?xml version="1.0" encoding="UTF-8"?>\n' + ss
 
 
+def _get_map(places):
+    def style_list(lname, color):
+        if color is None:
+            return lname
+        else:
+            return f'<span style="color:{color}">{lname}</span>'
+
+    callback = """
+function (row) {
+    const [lat, lon, color, tooltip, popup] = row;
+    // const marker = L.marker(
+    //     new L.LatLng(lat, lon),
+    //     {
+    //         color: color,
+    //     },
+    // );
+    const marker = L.circleMarker(
+        new L.LatLng(lat, lon),
+        {radius: 7,
+         color: color,
+         fill: true,
+         fill_color: color,
+       }
+    );
+    marker.bindPopup(popup);
+    marker.bindTooltip(tooltip, {permanent: true});
+    //  TODO get url from 4sq?
+    return marker;
+};
+"""
+    params = []
+    for p in places:
+        tooltip = p.name + ' ' + style_list(p.lst, p.color)
+        popup = '<br>'.join(p.descripton.splitlines())
+        params.append([p.lat, p.lng, p.color, tooltip, popup])
+
+    import folium
+    from folium import plugins as fplugins
+    fmap = folium.Map(location=LONDON) # TODO perhaps extract in my.geo or something?
+    cluster = fplugins.FastMarkerCluster(
+        params,
+        callback=callback,
+    ).add_to(fmap)
+    fplugins.Fullscreen().add_to(fmap)
+
+
+    legend_parts = []
+    for lname, color in INTERESTING.items():
+        legend_parts.append(style_list(lname, color))
+
+    # https://medium.com/@bobhaffner/creating-a-legend-for-a-folium-map-c1e0ffc34373
+    legend_html = f"""
+     <div style=”position: fixed;
+     bottom: 50px; left: 50px; width: 100px; height: 90px;
+     border:2px solid grey; z-index:9999; font-size:14px;
+     “>&nbsp; Legend: {' '.join(legend_parts)}
+     </div>
+     """
+
+    fmap.get_root().html.add_child(folium.Element(legend_html))
+    return fmap
+
+
+def test_get_map():
+    with TemporaryDirectory() as td:
+        mm = Path(td) / 'map.html'
+        places = get_test_places()
+        _get_map(places).save(str(mm))
+
+
+def get_map():
+    places = list(gen_places())
+    # places = []
+    return _get_map(places)
+
+
 def main():
+    setup_logzero(logger, level=logging.INFO)
     pa = argparse.ArgumentParser()
     pa.add_argument('--kml', type=Path)
     pa.add_argument('--map', type=Path)
@@ -196,9 +327,11 @@ def main():
 
     if args.kml is not None:
         args.kml.write_text(get_kml())
-        print(f"File to upload:\n{args.kml}")
+        logger.info('file to upload: %s', args.kml)
     elif args.map is not None:
-        pass
+        fmap = get_map()
+        fmap.save(str(args.map))
+        logger.info('saved map: %s', args.map)
     else:
         raise RuntimeError(args)
 
